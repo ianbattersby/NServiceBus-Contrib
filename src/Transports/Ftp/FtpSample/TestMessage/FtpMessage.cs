8be3e786193269aaa2b2a971587258fe3d944d25
andreasohlund <andreasohlund2@gmail.com>
2013-02-15 12:37:54 +0100

    Adding the Ftp transport that used to reside in the core project

---
+++ src/Transports/Ftp/FtpSample/TestMessage/FtpMessage.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using NServiceBus;
+
+namespace TestMessage
+{
+    [Serializable]
+    public class FtpMessage : ICommand
+    {
+        public int Id { get; set;  }
+        public String Name { get; set; }
+    }
+}
---
+++ src/Transports/Ftp/FtpSample/TestServer/EndpointConfig.cs
@@ -0,0 +1,16 @@
+﻿using NServiceBus;
+
+namespace TestServer
+{
+    public class EndpointConfig : AsA_Server, IConfigureThisEndpoint, IWantCustomInitialization
+    {
+        public void Init()
+        {
+            Configure.With()
+                .DefaultBuilder()
+                .FtpTransport()
+                .UnicastBus()
+                    .LoadMessageHandlers();
+        }
+    }
+}
